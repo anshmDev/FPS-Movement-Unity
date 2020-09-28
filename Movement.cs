bd4effaa89ce879ad6286cd370fe0b4a8d6d0a06
Ansh Maru <69057488+anshmDev@users.noreply.github.com>
2020-09-28 17:50:46 -0400

    Update Movement.cs

--- Movement.cs
+++ Movement.cs
@@ -6,22 +6,25 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    
+    //this is the actual player while the transform is the player capsule
     public CharacterController player;
-
     public Transform playerCapsule;
     
     //some movement jump and speed variables
-    public float walkSpeed = 4f;
-    public float runSpeed = 6f;
-    public float crouchSpeed = 2f;
+    public float walkSpeed = 2f;
+    public float runSpeed = 4f;//the runSpeed and crouchSpeed variables are not being used rn. Planning to integrate it soo though
+    public float crouchSpeed = 0.5f;
     public float jumpHeight = 3f;
-    public float gravity = -9.81f;
-    public float maxSpeed = 10f;
+    public float gravity = -9.81f; //earths gravity is about 9.81 m/s^2, so this is the default value for the script.(if i removed the minus the player would float)
+    /*public float maxSpeed = 10f;/*
+    //This is not needed. This is in case the script has a conflict
 
     //stuff for the grounded stuff
     public Transform groundCheck;
     public float groundDistance = 2f;
     public LayerMask groundMask;
+    public bool isGrounded;
     
     //crouching stuff
     private bool m_Crouch = false;
@@ -31,10 +34,11 @@ public class Movement : MonoBehaviour
     private Vector3 crouchScale = new Vector3(1, 0.5f, 1);
     private Vector3 playerScale = new Vector3(1, 1f, 1);
     
-    //other shit
+    
+    //poor loner :(
     Vector3 velocity;
 
-    public bool isGrounded;
+    
     
     //START THE CODE 
     void Start()
@@ -51,7 +55,7 @@ public class Movement : MonoBehaviour
         Move();
         
         //see if the crouch keybind is pressed down
-        if (Input.GetKeyDown(crouchKey) && (isGrounded == true))
+        if (Input.GetKeyDown(crouchKey) && (isGrounded))
         {
             m_Crouch = !m_Crouch;
             
@@ -61,7 +65,7 @@ public class Movement : MonoBehaviour
     }
     
     
-    //the movement function to, u kno, move the player (includes jump)
+    //the movement function to, you know, move the player (includes jump functions)
     public void Move()
     {
         //this is all to check if the player is grounded or not
@@ -71,18 +75,31 @@ public class Movement : MonoBehaviour
         {
             velocity.y = -2f;
         }
-        //down to here is all the isgrounded stuff
+        
         
         //get the axes for the player to move
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        float xMove = Input.GetAxis("Horizontal");
+        float zMove = Input.GetAxis("Vertical");
         
         //get a vector3 to calculate where the player is going to move
-        Vector3 move = transform.right * x + transform.forward * z;
+        Vector3 move = transform.right * xMove + transform.forward * zMove;
+        
+        //Clamp the vectors magnitude to make sure that diagonal movement speed doesn't get doubled
 
-        player.Move(move * (walkSpeed * 3) * Time.deltaTime);
+        player.SimpleMove(Vector3.ClampMagnitude(move, 1.0f) * walkSpeed);
 
         
+        
+        /*some commented out code just in case I mess up or there is a conflict. this is basically a backup movement script
+        Vector3 forwardMovement = transform.forward * zMove;
+        Vector3 rightMovement = transform.right * xMove;
+        Vector3 movement = new Vector3(xMove, 0, zMove);
+        */
+        
+        //here as well
+        /*player.Move(move * (walkSpeed * 3) * Time.deltaTime);*/
+
+        //stuff for jumping
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
@@ -92,9 +109,11 @@ public class Movement : MonoBehaviour
 
         player.Move(velocity * Time.deltaTime);
         
+        
+
     }
     
-    //this is just to check if the player should be crouching or not
+    //this is just to check if the player should be crouching or not, and then scale the player accordingly
     void CheckCrouch()
     {
         if (m_Crouch == true && isGrounded == true)
